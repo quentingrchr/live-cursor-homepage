a5de6b973fe17996c4a692a6a74008f72863bd22
quentingrchr <q.grancher@gmail.com>
2023-11-06 10:27:34 +0100

    wip

---
+++ back/src/app.ts
@@ -0,0 +1,67 @@
+import express from "express";
+import http from "http";
+import * as socketio from "socket.io";
+import cors from "cors"; // Import the cors package
+import { SocketEvents } from "../../shared";
+import { VIRTUAL_CURSOR_COLORS } from "./constants/cursor";
+
+const app = express();
+app.use(cors());
+
+const server = http.createServer(app);
+const io = new socketio.Server(server, {
+  cors: {
+    origin: "*",
+  },
+});
+
+interface CursorPosition {
+  x: number;
+  y: number;
+}
+
+interface CursorData extends CursorPosition {
+  id: string;
+  color: string;
+}
+
+app.get("/", (req, res) => {
+  res.send("Hello, this is your TypeScript Express server!");
+});
+
+const cursorsData: Record<string, CursorData> = {};
+
+io.on(SocketEvents.Connection, (socket) => {
+  console.log(`A user connected with socket id ${socket.id}`);
+  cursorsData[socket.id] = {
+    id: socket.id,
+    x: 0,
+    y: 0,
+    color:
+      VIRTUAL_CURSOR_COLORS[
+        Math.floor(Math.random() * VIRTUAL_CURSOR_COLORS.length)
+      ],
+  };
+
+  socket.on(SocketEvents.NewPosition, (data: CursorPosition) => {
+    cursorsData[socket.id] = { ...cursorsData[socket.id], ...data };
+
+    const cursorDataArray = Object.values(cursorsData);
+    io.emit(SocketEvents.PositionsUpdate, cursorDataArray as CursorData[]);
+  });
+
+  socket.on(SocketEvents.CursorClick, (data: CursorPosition) => {
+    console.log(`User ${socket.id} clicked`);
+    io.emit(SocketEvents.CursorClick, data);
+  });
+
+  socket.on(SocketEvents.Disconnect, () => {
+    delete cursorsData[socket.id];
+    console.log(`A user disconnected with socket id ${socket.id}`);
+  });
+});
+
+const PORT = process.env.PORT || 3001;
+server.listen(PORT, () => {
+  console.log(`Server is running on port ${PORT}`);
+});
---
+++ back/src/constants/cursor.ts
@@ -0,0 +1,11 @@
+export const VIRTUAL_CURSOR_COLORS = [
+  "#FF5EA9",
+  "#00FFA4",
+  "#00DFFF",
+  "#006BFF",
+  "#FF00E5",
+  "#FF8C00",
+  "#B200FF",
+  "#E53D00",
+  "#00FF78",
+];
