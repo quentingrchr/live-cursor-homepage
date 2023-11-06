6d4d1ea48e9c5e21310196e8af448cca4a2993b2
quentingrchr <q.grancher@gmail.com>
2023-11-06 10:28:21 +0100

    wip

---
+++ shared/src/index.ts
@@ -0,0 +1,9 @@
+export default "foo";
+
+export enum SocketEvents {
+  Connection = "connection",
+  Disconnect = "disconnect",
+  NewPosition = "new_position",
+  PositionsUpdate = "positions_update",
+  CursorClick = "cursor_click",
+}
